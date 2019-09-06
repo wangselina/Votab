3663e483a085e5f74c18e0210bbb3438be6ee668
Selina Wang <selinawang199689@gmail.com>
2019-09-06 00:46:52 -0700

    remove ypl code as no longer available, add romanization functionality

--- src/main.js
+++ src/main.js
@@ -1,28 +1,122 @@
 $(document).ready(function () {
 
+    var hangul = {
+        vowels: [
+            "a",
+            "ae",
+            "ya",
+            "yae",
+            "eo",
+            "e",
+            "yeo",
+            "ye",
+            "o",
+            "wa",
+            "wae",
+            "oe",
+            "yo",
+            "u",
+            "wo",
+            "we",
+            "wi",
+            "yu",
+            "eu",
+            "ui",
+            "i"
+        ],
+        consonants: {
+            initial: [
+                "g",
+                "kk",
+                "n",
+                "d",
+                "tt",
+                "r",
+                "m",
+                "b",
+                "pp",
+                "s",
+                "ss",
+                "",
+                "j",
+                "jj",
+                "ch",
+                "k",
+                "t",
+                "p",
+                "h"
+            ],
+            final: [
+                "",
+                "k",
+                "k",
+                "gs",
+                "n",
+                "nj",
+                "nh",
+                "t",
+                "l",
+                "lg",
+                "lm",
+                "lb",
+                "ls",
+                "lt",
+                "lp",
+                "lh",
+                "m",
+                "p",
+                "bs",
+                "t",
+                "ss",
+                "ng",
+                "t",
+                "t",
+                "k",
+                "t",
+                "p",
+                "h" // ㅎ
+            ]
+        }
+    };
+
+    var hangulUnicodeStartIndex = 44032;
+    var hangulUnicodeEndIndex = 55203;
+
+    function isHangul(charUnicode) {
+        return (charUnicode >= hangulUnicodeStartIndex || charUnicode <= hangulUnicodeEndIndex)
+    }
+
+    function romanize(koreanText) {
+        let romanizedText = "";
+        for (let i = 0; i < koreanText.length; i++) {
+            let charUnicode = koreanText.charCodeAt(i);
+            if (isHangul(charUnicode)) {
+                charUnicode -= hangulUnicodeStartIndex;
+                let tail = charUnicode%28;
+                let vowel = ((charUnicode - tail)%588)/28;
+                let lead = Math.floor(charUnicode/588);
+                romanizedText += hangul.consonants.initial[lead];
+                romanizedText += hangul.vowels[vowel];
+                romanizedText += hangul.consonants.final[tail];
+            } else {
+                romanizedText += koreanText[i];
+            }
+        }
+        return romanizedText;
+    }
+
     $.getJSON('words.json', function (data) {
         randIndex = Math.floor(Math.random() * (data.length));
         wordEntry = data[randIndex];
 
         $("#word").text(wordEntry["word"]);
         $("#definition").text(wordEntry["definition"]);
+        $("#romanization").text(romanize(wordEntry["word"]));
 
-        const key = "AF68639F1BC475387E5D6E7853DA6993";
-        var encoded = encodeURIComponent(wordEntry["word"])
-        var api = "https://krdict.korean.go.kr/api/search?&key=" + key + "&q=" + encoded + "&part=exam";
-
-        var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + api + '"') + '&format=xml&callback=?';
-
-        $("body").addClass("loading");
-        $.getJSON(yql, function (data) {
-            let xmlDoc = $.parseXML(data.results[0]);
-            let example = $(xmlDoc).find("example").first();
-            $("#sentence").text(example.text());
-            $("body").removeClass("loading");
-        });
     });
 
     $(".footer-text").hover(function () {
         $(".footer-content").slideToggle("fast");
     });
 });
+
